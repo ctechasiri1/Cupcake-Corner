65d203f0d948c635ab236145f36e34a0d7b77fa9
Chiraphat <ctechasiri@gmail.com>
2025-01-25 21:23:08 -0800

    Initial Commit

---
+++ Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Chiraphat Techasiri on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
